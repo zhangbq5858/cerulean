072d63b42ab26b59a7a52fa4987757604980ec5a
jingyilin1995 <liuxuelinjingyi@163.com>
2018-03-11 16:01:22 -0700

    Update user.js

--- server/user.js
+++ server/user.js
@@ -4,7 +4,7 @@ const createUser = (id) => {
     let User = {};
     User.id = id;
     User.name = "User"+id;
-    User.votedUrls = [];
+    User.votedUrls = []; // save voted link's id
     return User;
 }
 
@@ -13,6 +13,7 @@ const initUsers = () => {
     let Users = {};
     for(let i = 0; i < 3; i++){
        let User =  createUser(i);
+       User.votedUrls.push(i+13);
        Users[User.id] = User;
     }
     return Users;
