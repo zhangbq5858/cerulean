1387f3b374cf1e2dd99c5279a9b2eae0f9b74ae6
jingyilin1995 <liuxuelinjingyi@163.com>
2018-03-10 23:53:38 -0800

    Update UserID

--- client/src/components/UserID.jsx
+++ client/src/components/UserID.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const UserID = ({ user }) => {
  // console.log("user part data check -> ", user);
   return (
-    <p className="userid-show">Welcome { user === null ? "" : user.id}!</p>
+    <p className="userid-show">Welcome { user === null ? "" : user.name}!</p>
   );
 };
 
