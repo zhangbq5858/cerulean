9ce91c4fec2138b8ddc570a30975b3ab786a9f87
jingyilin1995 <liuxuelinjingyi@163.com>
2018-03-11 16:03:46 -0700

    Update UrlList tags

--- client/src/components/UrlList.jsx
+++ client/src/components/UrlList.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import Button from './Button';
 
-const UrlList = ({ Link, buttonClickFunc, userId }) => {
+const UrlList = ({ Link, buttonClickFunc, user }) => {
 	// console.log("urllist part data check -> ", buttonClickFunc);
 
 	const clickFunc = (text) =>  {
-		return buttonClickFunc(text, Link.id, userId);
+		return buttonClickFunc(text, Link.id, user.id);
 	};
+
+	const voteOrUnvote = () => {
+		if(user.votedUrls.includes(Link.id)){
+			return "unvote";
+		}
+		return "vote";
+	}
 
 	return (
 		<li key={Link.id}>
 			<h4>{Link.title}</h4>
 			<a href={Link.url}>{Link.url}</a>
-			<span style={{color: "green"}}> {Link.tag} </span>
+			<span style={{color: "green"}}> {Link.tags.map((tag) => tag+" ")} </span>
 			<span>vote: {Link.vote} </span>
 			<Button
 				clickFunc={clickFunc}
-				text={"vote"}
+				text={voteOrUnvote()}
 			/>
 			<Button
 				clickFunc={clickFunc}
