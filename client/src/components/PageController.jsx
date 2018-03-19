b30bd60a563ac28b39275873c21507219388984b
zhangbq5858 <949233293@qq.com>
2018-03-18 17:20:09 -0700

    fix page next bug

--- client/src/components/PageController.jsx
+++ client/src/components/PageController.jsx
@@ -4,6 +4,9 @@ const debug = false;
 
 const PageController = ({  currentPage, totalPage, prePage, nextPage, setPage}) => {
    if(debug) console.log("PageController data check -> ", totalPage);
+
+   const page = document.querySelector('.page')
+
   const checkForSubmit = (event) => {
     if(event.key === "Enter") {
       if(debug) console.log("jump", event.target.value);
@@ -12,9 +15,19 @@ const PageController = ({  currentPage, totalPage, prePage, nextPage, setPage}) 
     }
   }
 
+  const onClickPrePage = (event) => {
+    page.value = "";
+    prePage(event);
+  }
+
+  const onClickNextPage = (event) => {
+    page.value = "";
+    nextPage(event);
+  }
+
   return (
       <div className="page-button">
-        <button onClick={prePage}>Prev</button><input placeholder={currentPage} onKeyPress={checkForSubmit}/> / {totalPage}<button onClick={nextPage}>Next</button>
+        <button onClick={onClickPrePage}>Prev</button><input className="page" placeholder={currentPage} onKeyPress={checkForSubmit}/> / {totalPage}<button onClick={onClickNextPage}>Next</button>
       </div>
   );
 };
